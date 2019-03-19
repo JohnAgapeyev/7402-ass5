47376f0fd0283308f61ecfbb676f49e8f7c30096
John Agapeyev <jagapeyev@gmail.com>
2019-03-18 19:11:59 -0700

    Moved ecb out to functions

---
+++ feistel.py
@@ -0,0 +1,83 @@
+#!/bin/python3
+
+import sys
+
+def pkcs7_pad(x):
+    padding = 16 - ((len(x) % 16 != 0) * (len(x) % 16))
+    return x + bytes([padding]) * padding
+
+def pkcs7_strip(x):
+    for i in range(x[-1]):
+        if x[-(i + 1)] != x[-1]:
+            raise ValueError('Input is not padded or padding is corrupt')
+    return x[:-x[-1]]
+
+#This is completely arbitrary, and bad
+def f(i, k, x):
+    for elem in x:
+        elem *= i
+        elem <<= k
+    return x
+
+def round(i, k, L, R):
+    return R, [a ^ b for (a,b) in zip(L, f(i, k, R))]
+
+def process_block(B, rounds, subkeys):
+    #Split the block
+    L, R = B[:8], B[8:]
+    for j in rounds:
+        L, R = round(j, subkeys[j], L, R)
+    return R + L
+
+def ecb_encrypt(plain, subkeys):
+    #i is block num
+    for i in range(len(plain) // 16):
+        start_block = i * 16
+        end_block = start_block + 16
+        #Grab the block
+        B = plain[start_block : end_block]
+        B = process_block(B, range(round_count), subkeys)
+        #Write the block back
+        plain[start_block : end_block] = B
+    return plain
+
+def ecb_decrypt(plain, subkeys):
+    #i is block num
+    for i in range(len(plain) // 16):
+        start_block = i * 16
+        end_block = start_block + 16
+        #Grab the block
+        B = plain[start_block : end_block]
+        B = process_block(B, reversed(range(round_count)), subkeys)
+        #Write the block back
+        plain[start_block : end_block] = B
+    plain = pkcs7_strip(plain)
+    return plain
+
+# Args are [mode] [input filename] [output filename]
+# mode is 'e' for encrypt, else decrypt
+if __name__ == '__main__':
+    if len(sys.argv[1:]) != 3:
+        print("give me args!")
+        sys.exit(1)
+
+    round_count = 8
+
+    #Master secret key
+    K = 7
+
+    #Subkey generation, not really lol
+    k = [K] * round_count
+
+    if sys.argv[1] == 'e':
+        P = pkcs7_pad(bytearray(open(sys.argv[2], 'rb').read()))
+        P = ecb_encrypt(P, k);
+        with open(sys.argv[3], 'wb') as out:
+            out.write(P)
+    else:
+        P = bytearray(open(sys.argv[2], 'rb').read())
+        if len(P) % 16 != 0:
+            raise ValueError('Ciphertext is not a valid length, it must be corrupted')
+        P = ecb_decrypt(P, k)
+        with open(sys.argv[3], 'wb') as out:
+            out.write(P)
