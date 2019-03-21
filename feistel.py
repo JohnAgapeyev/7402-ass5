ff048233fb0a8c4c7b9c2631541091cb6d5e57fd
Isaac Morneau <isaacmorneau@gmail.com>
2019-03-20 19:36:54 -0700

    fixed help message

--- feistel.py
+++ feistel.py
@@ -266,9 +266,12 @@ if __name__ == '__main__':
     def print_help():
         print('''usage:
     ./feistel.py [function] [mode] [quality] [input filename] [output filename]
-            function is 'e' for encrypt, 'd' for decrypt, 't' for test
+            function is 'e' for encrypt, 'd' for decrypt
             mode is 'ecb' for ecb, 'cbc' for cbc, and 'ctr' for ctr
-            quality is 'e' for easy, 'm' for medium, 'h' for hard''')
+            quality is 'e' for easy, 'm' for medium, 'h' for hard
+
+    ./feistel.py t [input filename]
+            runs automated tests to compare the different modes using a given file''')
         sys.exit(1)
     if len(sys.argv) == 1:
         print_help()
